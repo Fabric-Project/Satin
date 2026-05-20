edcf923022091bd76acd945d96e77df1870ee9d2
vade <vade@vade.info>
2026-05-20 01:57:54 -0400

    latest changes

--- Example/Example/Renderers/AR/ARPBRRenderer.swift
+++ Example/Example/Renderers/AR/ARPBRRenderer.swift
@@ -461,7 +461,9 @@ final class ARPBRRenderer: BaseRenderer, MaterialDelegate {
                 session.remove(anchor: existingAnchor)
             }
 
-            modelContainer.anchor = anchor
+            schedule { [weak self] in
+                self?.modelContainer.anchor = anchor
+            }
         }
     }
 
